11836c3de0d638db4593b19e1422230161560316
Adam <Adam@DESKTOP-4T4EU3E>
2025-03-30 16:40:13 +0200

    Add project files.

---
+++ BlogPlatform.Core/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace BlogPlatform.Core.Entities
+{
+    public class User:IdentityUser
+    {
+        public string FirstName { get; set; }=null!;
+        public string LastName { get; set; } = null!;
+
+        // 
+        public ICollection<Post> Posts { get; set; }
+        public ICollection<Comment> Comments { get; set; }
+    }
+}
